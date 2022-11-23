9c6482603c0ee0ece58e1a26bc47ddaaa6a72a72
Mateusz <zubermateusz@gmail.com>
2022-11-23 12:45:11 +0100

    add isBuzz (number %7 or last digit==7; isDuck inside number is 0 (not only started with 0)

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -17,20 +17,29 @@ public class Main {
         System.out.println("Properties of " + number);
 
         if (isEven(number)) { // check is even
-            System.out.println("even: true");
+            System.out.println("        even: true");
         } else {
-            System.out.println("even: false");
+            System.out.println("        even: false");
         }
         if (!isEven(number)) { // check is odd
-            System.out.println("odd: true");
+            System.out.println("         odd: true");
         } else {
-            System.out.println("odd: false");
+            System.out.println("         odd: false");
         }
 
+        if (isBuzz(number)) {
+            System.out.println("        buzz: true");
+        } else {
+            System.out.println("        buzz: false");
+        }
 
+        if (isDuck(number)) {
+            System.out.println("        duck: true");
+        } else {
+            System.out.println("        duck: false");
+        }
 
-
-        // number is Buzz number if last digit == 7 or number % 7 == 0
+/*      // number is Buzz number if last digit == 7 or number % 7 == 0
         if (number % 7 == 0 || number % 10 == 7) {
             if (number % 7 == 0 && number % 10 == 7) {
                 System.out.println("It is a Buzz number.");
@@ -53,7 +62,33 @@ public class Main {
             System.out.println("Explanation:");
             System.out.println(number+ " is neither divisible by 7 nor does it end with 7.");
         }
+
+ */ //STAGE 1/8
     }
+
+    private static boolean isDuck(int number) {
+        boolean isDuckNumber = false;
+        StringBuilder numberInText = new StringBuilder();
+        numberInText.append(number);
+        for (int i = 1; i < numberInText.length(); i++) {
+            if (numberInText.charAt(i) == '0') {
+                isDuckNumber = true;
+                break;
+            }
+        }
+        return isDuckNumber;
+    }
+
+    private static boolean isBuzz(int number) {
+        boolean isBuzzNumber = false;
+        if (number % 7 == 0) {
+            isBuzzNumber = true;
+        }
+        if (number % 10 == 7) {
+            isBuzzNumber = true;
+        }
+        return isBuzzNumber;
+    } // check the number is Buzz number
 
 
     private static boolean isEven(int number) {
