aa76d1e3d3b34c87709ed80ea3ee09dc6ea485f1
Mateusz <zubermateusz@gmail.com>
2022-11-23 14:19:44 +0100

    STAGE 3/8 add menu, add isPalindromic (1001)

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -5,40 +5,70 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        write your code here
-        System.out.println("Enter a natural number:");
+
+        System.out.println("Welcome to Amazing Numbers!");
+        System.out.println();
+        System.out.println("Supported requests:\n" +
+                "- enter a natural number to know its properties;\n" +
+                "- enter 0 to exit.");
+        System.out.println();
+
+
+
+        // System.out.println("Enter a natural number:"); STAGE 2/8
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        //check number is not natural
-        if (!isNatural(number)) {
-            System.out.println("This number is not natural!");
-            System.exit(0);
-        }
 
-        System.out.println("Properties of " + number);
 
-        if (isEven(number)) { // check is even
-            System.out.println("        even: true");
-        } else {
-            System.out.println("        even: false");
-        }
-        if (!isEven(number)) { // check is odd
-            System.out.println("         odd: true");
-        } else {
-            System.out.println("         odd: false");
-        }
+        for(;;) {
+            System.out.print("Enter a request: ");
+            long number = scanner.nextLong();
+            System.out.println(); // new line after enter the number
+            if (number == 0) {
+                System.out.println("Goodbye!");
+                System.exit(0);
+            } // exit
 
-        if (isBuzz(number)) {
-            System.out.println("        buzz: true");
-        } else {
-            System.out.println("        buzz: false");
-        }
+            if (!isNaturalor0(number)) {
+                System.out.println("The first parameter should be a natural number or zero.");
+                System.out.println();
+                // STAGE2/8System.out.println("This number is not natural!");
+                // STAGE2/8System.exit(0);
+            } //check number is not natural
+            else {
 
-        if (isDuck(number)) {
-            System.out.println("        duck: true");
-        } else {
-            System.out.println("        duck: false");
-        }
+                System.out.println("Properties of " + number);
 
+                if (isEven(number)) { // check is even
+                    System.out.println("        even: true");
+                } else {
+                    System.out.println("        even: false");
+                }
+                if (!isEven(number)) { // check is odd
+                    System.out.println("         odd: true");
+                } else {
+                    System.out.println("         odd: false");
+                }
+
+                if (isBuzz(number)) { // check isBuzz Number
+                    System.out.println("        buzz: true");
+                } else {
+                    System.out.println("        buzz: false");
+                }
+
+                if (isDuck(number)) { // check isDuck number
+                    System.out.println("        duck: true");
+                } else {
+                    System.out.println("        duck: false");
+                }
+
+                if (isPalindromic(number)) { // check ispPlindromic
+                    System.out.println(" palindromic: true");
+                } else {
+                    System.out.println(" palindromic: false");
+                }
+                System.out.println(); // new line at the end
+            }
+        }
 /*      // number is Buzz number if last digit == 7 or number % 7 == 0
         if (number % 7 == 0 || number % 10 == 7) {
             if (number % 7 == 0 && number % 10 == 7) {
@@ -66,7 +96,20 @@ public class Main {
  */ //STAGE 1/8
     }
 
-    private static boolean isDuck(int number) {
+    private static boolean isPalindromic(long number) {
+        boolean isPalindromicNumber = true;
+        String[] arrayOfNumber = ("" + number).split("");
+        for (int i = 0, k = arrayOfNumber.length - 1; i < arrayOfNumber.length / 2; i++, k--) {
+            if (!arrayOfNumber[i].equals(arrayOfNumber[k])) {
+                isPalindromicNumber = false;
+                break;
+            }
+
+        }
+        return isPalindromicNumber;
+    }
+
+    private static boolean isDuck(long number) {
         boolean isDuckNumber = false;
         StringBuilder numberInText = new StringBuilder();
         numberInText.append(number);
@@ -79,7 +122,7 @@ public class Main {
         return isDuckNumber;
     }
 
-    private static boolean isBuzz(int number) {
+    private static boolean isBuzz(long number) {
         boolean isBuzzNumber = false;
         if (number % 7 == 0) {
             isBuzzNumber = true;
@@ -91,11 +134,11 @@ public class Main {
     } // check the number is Buzz number
 
 
-    private static boolean isEven(int number) {
+    private static boolean isEven(long number) {
         return number % 2 == 0;
     } // check is odd or even
 
-    private static boolean isNatural(int number) {
-        return number > 0;
+    private static boolean isNaturalor0(long number) {
+        return number >= 0;
     } // check is Natural
 }
