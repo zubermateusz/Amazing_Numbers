3796e30a2908fb0973bcc157b5ce5fb45a84169a
Mateusz <zubermateusz@gmail.com>
2022-12-15 21:35:01 +0100

    add UncheckedException

---
+++ Topics/Hierarchy of exceptions/UncheckedException/src/Main.java
@@ -0,0 +1,13 @@
+// update the class
+class UncheckedException extends RuntimeException {
+
+    public UncheckedException(){}
+}
+
+//do not change the code
+class Main {
+    public static void main(String[] args) {
+        UncheckedException uncheckedException = new UncheckedException();
+        System.out.println(uncheckedException instanceof RuntimeException);
+    }
+}
